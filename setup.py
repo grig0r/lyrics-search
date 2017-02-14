3a8e0cadcf3a603126eb43c45488c0e6769c2429
grig0r <twojastaara@gmail.com>
2017-02-14 11:53:22 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='lyrics_search',
     version='0.1',
+    author='grig0r',
     description='find song lyrics on the web',
     packages=['lyrics_search'],
     )
